dd97091fde1625da1cca71f030311491a6aef02a
Techalone <techalone2@gmail.com>
2024-10-12 05:09:25 +1245

    3rd commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Button, Drawer, List, ListItem, ListItemText, Divider, useMediaQuery } from '@mui/material';
-import CodeIcon from '@mui/icons-material/Code';
+import { AppBar, Toolbar, Typography, Button, Drawer, List, ListItem, ListItemText, Divider, useMediaQuery, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'; // Import the Menu icon
 import LogoImage from '../../src/images/logo.png'; // Replace with your actual logo image path
 
 const Navbar = ({ onHeroClick, onProjectsClick, onExperienceClick, onSkillsClick, onServicesClick, onTestimonialClick, onFooterClick }) => {
@@ -35,9 +35,11 @@ const Navbar = ({ onHeroClick, onProjectsClick, onExperienceClick, onSkillsClick
         </ListItem>  
         <ListItem button onClick={onServicesClick}>
           <ListItemText primary="Services" />
-        </ListItem> <ListItem button onClick={onTestimonialClick}>
+        </ListItem>
+        <ListItem button onClick={onTestimonialClick}>
           <ListItemText primary="Testimonials" />
-        </ListItem>   <ListItem button onClick={onFooterClick}>
+        </ListItem>   
+        <ListItem button onClick={onFooterClick}>
           <ListItemText primary="RequestQuote" />
         </ListItem>     
       </List>
@@ -49,24 +51,22 @@ const Navbar = ({ onHeroClick, onProjectsClick, onExperienceClick, onSkillsClick
     <div>
       <AppBar position="static" sx={{ backgroundColor: '#1b77bb' }}>
         <Toolbar>
-          <img src={LogoImage} alt="Logo" style={{ width: '40', height: '40px', marginRight: '10px' }} /> {/* Logo image */}
+          <img src={LogoImage} alt="Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} /> {/* Logo image */}
           <Typography variant="h6" component="div" sx={{ mr: 'auto' }}>
             <a href="/" style={{ textDecoration: 'none', color: 'inherit' }} onClick={onHeroClick}>
               SF Ali Travel Services
             </a>
           </Typography>
           {isSmallScreen ? (
-            <Button onClick={toggleDrawer(true)}>
-              Menu
-            </Button>
+            <IconButton edge="end" color="inherit" onClick={toggleDrawer(true)}>
+              <MenuIcon /> {/* Menu Icon */}
+            </IconButton>
           ) : (
             <Toolbar sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              {/* <Button color="inherit" sx={buttonStyle} onClick={onExperienceClick}>Experience</Button>
-              <Button color="inherit" sx={buttonStyle} onClick={onSkillsClick}>Skills</Button>
-              <Button color="inherit" sx={buttonStyle} onClick={onProjectsClick}>Projects</Button> */}
               <Button color="inherit" sx={buttonStyle} onClick={onHeroClick}>Home</Button>
-              <Button color="inherit" sx={buttonStyle} onClick={onServicesClick}>Services</Button>              <Button color="inherit" sx={buttonStyle} onClick={onTestimonialClick}>Testimonials</Button>
-              <Button color="inherit" sx={buttonStyle} onClick={onFooterClick}>Contact us</Button>   
+              <Button color="inherit" sx={buttonStyle} onClick={onServicesClick}>Services</Button>
+              <Button color="inherit" sx={buttonStyle} onClick={onTestimonialClick}>Testimonials</Button>
+              <Button color="inherit" sx={buttonStyle} onClick={onFooterClick}>Contact us</Button>
             </Toolbar>
           )}
         </Toolbar>
